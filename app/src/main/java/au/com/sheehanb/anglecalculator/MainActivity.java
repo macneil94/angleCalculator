6251497055879020190a01acc95c3ca3aa2e70c4
Ben S <sheebe12@gmail.com>
2019-03-30 13:24:52 +1000

    adding some calc functions

--- app/src/main/java/au/com/sheehanb/anglecalculator/MainActivity.java
+++ app/src/main/java/au/com/sheehanb/anglecalculator/MainActivity.java
@@ -2,17 +2,19 @@ package au.com.sheehanb.anglecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private EditText aEditText;
     private EditText bEditText;
     private EditText cEditText;
     private EditText dEditText;
     private EditText eEditText;
+
+    public final String LOG_TAG = "angle_calculator";
 
     /*
     String aString = aEditText.getText().toString();
@@ -36,6 +38,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void calcAngle(View view) {
         System.out.println("test");
+        System.out.println("aEditText.getText() = " + aEditText.getText());
+
+        double width = Double.parseDouble(aEditText.getText().toString());
+        double angle = Double.parseDouble(dEditText.getText().toString());
+
+        double backcut = calc_backcut(width, angle);
+        double cutlength = calc_cutlength(width, angle);
+
+        Log.d(LOG_TAG, "backcut = " + backcut);
+        Log.d(LOG_TAG, "cutlength = " + cutlength);
+    }
+
+    public void update_fields(){}
+
+    public double calc_backcut(double a, double d) {
+        Log.d("angle_calculator", "calc_backcut: a=" + a + " d=" + d);
+
+        d = Math.toRadians(d);
+
+        double tanD = Math.tan(d);
+
+        return a / tanD;
+    }
+
+    public double calc_cutlength(double a, double d) {
+        Log.d(LOG_TAG, "calc_cutlength: a=" + a + " d=" + d);
+        return a / (Math.sin(d));
     }
 
     private void updateFigures(){}
